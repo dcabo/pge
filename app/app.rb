87df1da59e7eb3f5d108f0ff27d42d6538ca510e
David Cabo <david@calibea.com>
2010-10-04 01:08:27 +0200

    added top level by programme view

--- app/app.rb
+++ app/app.rb
@@ -11,6 +11,11 @@ class StateBudgetApp < Sinatra::Base
     @sections = Expense.all(:entity_id => '')
     haml :index
   end
+
+  get '/by_section' do
+    @sections = Expense.all(:entity_id => '')
+    haml :by_section
+  end
   
   get '/section/:section' do
     @section = Expense.all(:section => params[:section], :entity_id => '').first
@@ -31,6 +36,42 @@ class StateBudgetApp < Sinatra::Base
     @entity = Expense.all(:section => params[:section], :entity_id => params[:entity], :programme => '').first
     @programme = Expense.all(:section => params[:section], :entity_id => params[:entity], :programme => params[:programme], :concept => '').first
     @expenses = Expense.all(:section => params[:section], :entity_id => params[:entity], :programme => params[:programme], :concept.not => '')
+    haml :programme
+  end
+  
+  get '/by_programme' do
+    # Since a program can be split across many entities, we need to consolidate the list and add up the amounts.
+    # Surprisingly, there doesn't seem to be a way to do this in DataMapper
+    @programmes = {}
+    Expense.all(:programme.not => '', :concept => '').each do |p|
+      if (previous=@programmes[p.programme]).nil? 
+        @programmes[p.programme] = p 
+      else
+        previous.amount += p.amount
+      end
+    end
+    @programmes = @programmes.values.sort {|a,b| a.programme <=> b.programme }
+    haml :by_programme
+  end
+  
+  get '/programme/:programme' do
+    @programme = Expense.all(:programme => params[:programme], :concept => '').first
+    
+    # Since a program can be split across many entities, we need to consolidate the expense list and add up the amounts
+    # TODO: Do we want to show also the expenses split per entity?
+    @expenses = {}
+    Expense.all(:programme => params[:programme], :concept.not => '').each do |e|
+      # Note that for the same 'economic concept code' we may have more than one description *sigh* so,
+      # in order not to lose information, we group by concept _and_ description
+      key = e.concept+e.description
+      
+      if (previous=@expenses[key]).nil? 
+        @expenses[key] = e
+      else
+        previous.amount += e.amount
+      end
+    end
+    @expenses = @expenses.values.sort {|a,b| a.concept <=> b.concept }
     haml :programme
   end
 end
