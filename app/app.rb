c6cb8837b58b1a31c7d24fd4fa5ea0515dcb4f4e
David Cabo <david@calibea.com>
2010-10-03 23:13:55 +0200

    adding programme view page

--- app/app.rb
+++ app/app.rb
@@ -18,10 +18,19 @@ class StateBudgetApp < Sinatra::Base
     haml :section
   end
   
+  # TODO: Move all this logic to model
   get '/section/:section/entity/:entity' do
     @section = Expense.all(:section => params[:section], :entity_id => '').first
     @entity = Expense.all(:section => params[:section], :entity_id => params[:entity], :programme => '').first
     @programmes = Expense.all(:section => params[:section], :entity_id => params[:entity], :programme.not => '', :concept => '')
     haml :entity
+  end
+  
+  get '/section/:section/entity/:entity/programme/:programme' do
+    @section = Expense.all(:section => params[:section], :entity_id => '').first
+    @entity = Expense.all(:section => params[:section], :entity_id => params[:entity], :programme => '').first
+    @programme = Expense.all(:section => params[:section], :entity_id => params[:entity], :programme => params[:programme], :concept => '').first
+    @expenses = Expense.all(:section => params[:section], :entity_id => params[:entity], :programme => params[:programme], :concept.not => '')
+    haml :programme
   end
 end
