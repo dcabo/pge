c4a2b681b6e1b1d1d0f2932aa7f804dcf2da8ca8
David Cabo <david@calibea.com>
2010-10-03 06:23:49 +0200

    extract expense subtotals by matching opening/closing headings

--- extract_expenses.rb
+++ extract_expenses.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/ruby
 
+# Budget expenses are organized in chapters > articles > concepts > subconcepts. When looking at 
+# an expense breakdown, the sum of all the chapters (codes of the form 'n') equals the sum of all 
+# articles (codes 'nn') and the sum of all expenses (codes 'nnn'). I.e. the breakdown is exhaustive 
+# down to that level. Note however that not all concepts are broken into sub-concepts (codes 'nnnnn'); 
+# hence, adding up all the subconcepts will result in a much smaller amount.
+
 require 'filenames'
 require 'economic_breakdown'
 
@@ -10,21 +16,38 @@ end
 puts 'section, service, programme, concept, description, amount'
 Dir["PGE-ROM/doc/HTM/*.HTM"].each {|filename|
   if ( filename =~ STATE_ENTITY_EXPENSES_ECON_BKDOWN )
-    next if $2 != '17'
+    bkdown = EconomicBreakdown.new(filename)
+    
+    # The total amounts for service/programme/chapter headings is shown when the heading is closed,
+    # not opened, so we need to keep track of the open ones.
+    # Note: there is an unmatched closing amount, without an opening heading, at the end
+    # of the page, containing the amount for the whole section/entity, so we don't start with
+    # an empty vector
+    open_headings = ["#{bkdown.section}|#{bkdown.entity_type}|||#{bkdown.name}"]
     
     service = ''
     programme = ''
-    expense_concept = ''
-    bkdown = EconomicBreakdown.new(filename)
     bkdown.rows.each {|row|
-      service = row[:service] if not row[:service].empty?
-      programme = row[:programme] if not row[:programme].empty?
-      expense_concept = row[:expense_concept] if not row[:expense_concept].empty?
+      next if row[:description].empty?  # Skip empty lines
       
-      # Skip service/programme/chapter headings, we only want the details
-      next if row[:amount].empty? or row[:expense_concept].empty?
+      # Keep track of current service/programme
+      if not row[:service].empty?
+        service = row[:service] 
+        programme = ''
+      elsif not row[:programme].empty?
+        programme = row[:programme] 
+      end
       
-      puts "#{bkdown.section}|#{service}|#{programme}|#{expense_concept}|#{row[:description]}|#{convert_number(row[:amount])}"
+      # Print expense
+      expense_description = "#{bkdown.section}|#{bkdown.entity_type}|#{service}|#{programme}|#{row[:expense_concept]}|#{row[:description]}"
+      if ( row[:amount].empty? )              # opening heading
+        open_headings << expense_description
+      elsif ( row[:expense_concept].empty? )  # closing heading
+        last_heading = open_headings.pop()
+        puts "#{last_heading}|#{convert_number(row[:amount])}" unless last_heading.empty?
+      else                                    # standard data row
+        puts "#{expense_description}|#{convert_number(row[:amount])}"
+      end
     }
   end
 }
