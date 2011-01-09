6bbd818eaf8d331f716c05696428a42d2c6a2433
David Cabo <david@calibea.com>
2011-01-09 14:30:48 +0100

    added TODOs

--- parser/extract_child_sections.rb
+++ parser/extract_child_sections.rb
@@ -3,6 +3,7 @@
 require 'budget'
 require 'economic_breakdown'
 
+# TODO: Missing Social Security here
 puts 'Id,Sección,Tipo,Organismo'
 Budget.new(ARGV[0]).economic_breakdowns.each do |bkdown|
   bkdown.children.each do |child|
--- parser/extract_top_level_sections.rb
+++ parser/extract_top_level_sections.rb
@@ -3,6 +3,7 @@
 require 'budget'
 require 'economic_breakdown'
 
+# TODO: Missing Social Security here
 puts 'Id, Nombre'
 Budget.new(ARGV[0]).economic_breakdowns.each do |bkdown| 
   puts "#{bkdown.section},#{bkdown.name}" if bkdown.is_state_entity
